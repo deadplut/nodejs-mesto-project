de75e440e7c11a47d6252d4da862fcd70f3255e3
Suganov Boris <suganov@t8.ru>
2024-11-16 17:06:09 +0300

    first step

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,25 +1,38 @@
-import { model, Schema } from 'mongoose';
+import { Schema, model } from 'mongoose';
+import validator from 'validator';
 
 export interface IUser {
   name: string;
   about: string;
   avatar: string;
+  email: string;
+  password: string;
 }
 
 const UserSchema = new Schema<IUser>({
   name: {
     type: String,
     minlength: 2,
-    maxlength: 30,
-    required: true
+    maxlength: 30
   },
   about: {
     type: String,
     minlength: 2,
-    maxlength: 200,
-    required: true
+    maxlength: 200
   },
   avatar: {
+    type: String
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v: string) => validator.isEmail(v),
+      message: 'Неправильный формат почты'
+    }
+  },
+  password: {
     type: String,
     required: true
   }
